6d072bf5386a32dbceb63f8e92e3713ec3f02a27
rishij-at-803992426592 <rishi18304@iiitd.ac.in>
2020-06-25 09:37:09 +0530

    Added meta properties and names.

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, AfterContentInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
@@ -24,7 +25,9 @@ export class BlogComponent implements OnInit, AfterContentInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private http: HttpClient
+    private http: HttpClient,
+    private meta: Meta,
+    private titleService: Title
   ) { }
 
   ngOnInit(): void {
@@ -41,6 +44,7 @@ export class BlogComponent implements OnInit, AfterContentInit {
       console.log(this.intro);
     }
     else{
+      console.log('Here');
       this.http.get('https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40s_eschweiler')
       .subscribe(
         (r)=>{
@@ -48,10 +52,75 @@ export class BlogComponent implements OnInit, AfterContentInit {
           let response= JSON.parse(JSON.stringify(r));
           let blogs= response['items'];
           let thisblog= blogs.filter(ele => ele.title===this.id)[0];
-          console.log(thisblog);
           this.title= thisblog['title'];
           this.thumbnail= thisblog['thumbnail'];
           this.content= thisblog['content'];
+          this.titleService.setTitle(this.title);
+          let title= this.title;
+          let desc= this.content;
+          let timeofcreation= thisblog['pubDate'];
+          let author= thisblog['author'];
+          let urltoshare= this.router.url;
+          this.meta.updateTag({name: 'canonical', content: urltoshare});
+          this.meta.updateTag({property: 'al:web:url', content: urltoshare});
+          this.meta.updateTag({property: 'al:web:url', content: urltoshare});
+          this.meta.updateTag({property: 'og:url', content: urltoshare});
+          this.meta.updateTag({property: 'canonical', content: urltoshare});
+          this.meta.updateTag({name: 'al:web:url', content: urltoshare});
+          this.meta.updateTag({name: 'al:web:url', content: urltoshare});
+          this.meta.updateTag({name: 'og:url', content: urltoshare});
+          this.meta.updateTag({name: 'title', content: title});
+          this.meta.updateTag({name: 'description', content: desc});
+          this.meta.updateTag({name: 'twitter:app:name:iphone', content: 'company_name'});
+          this.meta.updateTag({name: 'twitter:app:id:iphone', content: 'id_number'});
+          this.meta.updateTag({name: 'al:ios:app_name', content: 'company_name'});
+          this.meta.updateTag({name: 'al:ios:app_store_id', content: 'id_number'});
+          this.meta.updateTag({name: 'al:android:package', content: 'com.company_name.company_namerx'});
+          this.meta.updateTag({name: 'og:site_name', content: 'company_name'});
+          this.meta.updateTag({name: 'og:type', content: 'article'});
+          this.meta.updateTag({name: 'article:published_time', content: timeofcreation});
+          this.meta.updateTag({name: 'og:title', content: title});
+          this.meta.updateTag({name: 'twitter:title', content: title});
+          this.meta.updateTag({name: 'twitter:site', content: '@company_name'});
+          this.meta.updateTag({name: 'og:description', content: desc});
+          this.meta.updateTag({name: 'twitter:description', content: desc});
+          this.meta.updateTag({name: 'twitter:site', content: '@company_name'});
+          this.meta.updateTag({name: 'og:description', content: desc});
+          this.meta.updateTag({name: 'twitter:description', content: desc});
+          this.meta.updateTag({name: 'twitter:card', content: 'summary'});
+          this.meta.updateTag({name: 'twitter:description', content: desc});
+          this.meta.updateTag({name: 'author', content: author});
+          this.meta.updateTag({name: 'robots', content: 'index, follow'});
+          this.meta.updateTag({name: 'referrer', content: 'unsafe-url'});
+          this.meta.updateTag({name: 'author', content: author});
+          this.meta.updateTag({name: 'article:author', content: 'https://medium.com/@company_name'});
+          this.meta.updateTag({name: 'twitter:creator', content: '@company_name'});
+          this.meta.updateTag({property: 'title', content: title});
+          this.meta.updateTag({property: 'description', content: desc});
+          this.meta.updateTag({property: 'twitter:app:name:iphone', content: 'company_name'});
+          this.meta.updateTag({property: 'twitter:app:id:iphone', content: 'id_number'});
+          this.meta.updateTag({property: 'al:ios:app_name', content: 'company_name'});
+          this.meta.updateTag({property: 'al:ios:app_store_id', content: 'id_number'});
+          this.meta.updateTag({property: 'al:android:package', content: 'com.company_name.company_namerx'});
+          this.meta.updateTag({property: 'og:site_name', content: 'company_name'});
+          this.meta.updateTag({property: 'og:type', content: 'article'});
+          this.meta.updateTag({property: 'article:published_time', content: timeofcreation});
+          this.meta.updateTag({property: 'og:title', content: title});
+          this.meta.updateTag({property: 'twitter:title', content: title});
+          this.meta.updateTag({property: 'twitter:site', content: '@company_name'});
+          this.meta.updateTag({property: 'og:description', content: desc});
+          this.meta.updateTag({property: 'twitter:description', content: desc});
+          this.meta.updateTag({property: 'twitter:site', content: '@company_name'});
+          this.meta.updateTag({property: 'og:description', content: desc});
+          this.meta.updateTag({property: 'twitter:description', content: desc});
+          this.meta.updateTag({property: 'twitter:card', content: 'summary'});
+          this.meta.updateTag({property: 'twitter:description', content: desc});
+          this.meta.updateTag({property: 'author', content: author});
+          this.meta.updateTag({property: 'robots', content: 'index, follow'});
+          this.meta.updateTag({property: 'referrer', content: 'unsafe-url'});
+          this.meta.updateTag({property: 'author', content: author});
+          this.meta.updateTag({property: 'article:author', content: 'https://medium.com/@company_name'});
+          this.meta.updateTag({property: 'twitter:creator', content: '@company_name'});
         },
         (e)=>{
           console.log(e);
